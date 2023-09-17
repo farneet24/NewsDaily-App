02afea18351db407cd70ba5d6a248b6cba82255d
farneet24 <111075591+farneet24@users.noreply.github.com>
2023-09-17 15:05:30 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const NewsItem = ({ title, description, ImageURL, NewsURL, author, date }) => {
-
-  const navigate = useNavigate();  // Note the change here
+const NewsItem = ({
+  title,
+  description,
+  ImageURL,
+  NewsURL,
+  author,
+  date,
+  dat,
+}) => {
+  const navigate = useNavigate(); // Note the change here
 
   const handleReadArticle = () => {
-    navigate("/article", { state: { title, description, ImageURL, NewsURL, author, date  } });  // Note the change here
+    navigate("/article", {
+      state: { title, description, ImageURL, NewsURL, author, date },
+    }); // Note the change here
+  };
+
+  const truncateString = (str, num) => {
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + "...";
   };
 
   const formatDate = (dateString) => {
@@ -34,38 +50,38 @@ const NewsItem = ({ title, description, ImageURL, NewsURL, author, date }) => {
     }
   };
 
-  console.log("Title and Description before Link:", title, description);
+  console.log("Title and Description before Link:", title, description, dat);
 
   return (
     <>
-      <div className="card my-2 d-flex flex-md-row flex-column align-items-start">
-        <div className="d-flex flex-fill flex-column flex-md-row">
-          <img
-            src={ImageURL}
-            className="card-img img-fluid fixed-size-img mb-2 mb-md-0"
-            alt="..."
-          />
-          <div
-            className="flex-grow-1 d-flex flex-column justify-content-between"
-            id="newscardtext"
-          >
-            <div>
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-            </div>
-          </div>
-        </div>
-        <div
-          className="d-flex align-items-center mt-2 mt-md-0"
-          id="readmorebutton"
-        >
-          <button onClick={handleReadArticle} className="btn btn-primary">
-            Read Article
-          </button>
-        </div>
+     <div className="card my-2 d-flex flex-column flex-md-row align-items-start">
+  <div className="d-flex flex-fill flex-column flex-md-row">
+    <img
+      src={ImageURL}
+      className="card-img img-fluid mb-2 mb-md-0 fixed-size-img"
+      alt="..."
+    />
+    <div
+      className="flex-grow-1 d-flex flex-column justify-content-between p-3"
+    >
+      <div>
+        <h5 className="card-title">{truncateString(title, 70)}</h5>
+        <p className="card-text">{truncateString(description, 350)}</p>
       </div>
+    </div>
+  </div>
+  <div
+    className="d-flex align-items-center mt-2 mt-md-0 p-3"
+  >
+    <button onClick={handleReadArticle} className="btn btn-primary">
+      Read Article
+    </button>
+  </div>
+</div>
+
     </>
   );
 };
 
 export default NewsItem;
+
