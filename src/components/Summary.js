5922e891a68a3f7e025f7ea048eb7ca80cc35a70
farneet24 <111075591+farneet24@users.noreply.github.com>
2023-09-18 01:59:49 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,22 +3,13 @@ import PropTypes from "prop-types";
 
 const Summary = ({ articleText }) => {
   const commentsDiv = useRef(null);
-  const commentsKey = useRef(null);
   const [shouldDisplaySummarization, setShouldDisplaySummarization] =
     useState(false);
-  const [shouldDisplayKeyword, setShouldDisplayKeyword] = useState(false);
   const openedSessions = new Set(); // To keep track of all session IDs
 
   const displaySummary = (message) => {
     if (commentsDiv.current) {
       commentsDiv.current.innerHTML += message.replace(/\/n/g, "<br/>");
-    }
-  };
-
-  const displayKeyword = (message) => {
-    if (commentsKey.current) {
-      console.log(message);
-      commentsKey.current.innerHTML += message.replace(/\/n/g, "<br/>");
     }
   };
 
@@ -91,78 +82,6 @@ const Summary = ({ articleText }) => {
   };
   
 
-  const handleKeywords = async () => {
-    // Existing logic...
-    setShouldDisplayKeyword(true);
-  
-    const sendData = async () => {
-      try {
-        const response = await fetch(
-          "https://newsdailyfarneet-9e2e933f25bb.herokuapp.com/storeComments/",
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ articleText: articleText }),
-          }
-        );
-  
-        const data = await response.json();
-        console.log("Response:", data);
-        const { sessionId } = data;
-  
-        const source = new EventSource(
-          `https://newsdailyfarneet-9e2e933f25bb.herokuapp.com/stream/${sessionId}/keywords/`
-        );
-  
-        // Check for duplicate session IDs
-        if (openedSessions.has(sessionId)) {
-          console.log("Duplicate session ID. Closing source.");
-          source && source.close();
-          return;
-        }
-  
-        openedSessions.add(sessionId); // Add to the set of opened sessions
-        commentsKey.current.innerHTML = "";
-  
-        source.onmessage = (event) => {
-          displayKeyword(event.data);
-          if (commentsKey.current) {
-            const textContent =
-              commentsKey.current.textContent || commentsKey.current.innerText;
-            const words = textContent.split(/\s+/).filter(Boolean);
-            if (words.length >= 1000) {
-              source.close(); // Close the source if words exceed 1000
-              displayKeyword("<span>Word limit reached. Closing stream...</span>");
-            }
-          }
-        };
-  
-        source.addEventListener(
-          "done",
-          () => {
-            console.log("EventSource 'done' event received");
-            source.close();
-          },
-          false
-        );
-  
-        source.onerror = (event) => {
-          console.error("EventSource failed:", event);
-          source.close();
-          displayKeyword("<span>Error occurred while fetching data.</span>");
-        };
-  
-      } catch (error) {
-        console.error("There was an error sending the data", error);
-      }
-    };
-  
-    sendData();
-  };
-  
-
   useEffect(() => {
     if (!articleText) {
       // Handle this by setting a loading or error state
@@ -172,7 +91,6 @@ const Summary = ({ articleText }) => {
     console.log("I am running");
     handleSummarize();
     console.log("I am running after summary");
-    // handleKeywords();
   }, [articleText]);
 
   return (
@@ -212,42 +130,6 @@ const Summary = ({ articleText }) => {
           ></div>
         </div>
       )}
-
-      {shouldDisplayKeyword && (
-        <div
-          style={{
-            background: "#1F1F1F",
-            borderRadius: "15px",
-            color: "#FFFFFF",
-            padding: "20px",
-            margin: "20px auto",
-            maxWidth: "100%",
-            boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <h1
-            style={{
-              paddingLeft: "0.5%",
-              marginBottom: "20px",
-              borderBottom: `3px solid ${"#3F3F3F"}`,
-              color: "#F1F1F1",
-            }}
-          >
-            Keywords
-          </h1>
-          <div
-            style={{
-              background: "#2E2E2E",
-              color: "#E1E1E1",
-              padding: "15px",
-              borderRadius: "10px",
-              fontSize: window.innerWidth <= 768 ? "0.72rem" : "1rem",
-              lineHeight: "1.6",
-            }}
-            ref={commentsKey}
-          ></div>
-        </div>
-      )}
     </>
   );
 };
@@ -257,6 +139,7 @@ Summary.propTypes = {
 };
 
 export default Summary;
+
 
 
 
